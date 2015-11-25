d2c9c79cb47413bd78cdf695042f47749b023f1b
Gigakaiser <Brandon.Hjelstrom@gmail.com>
2015-11-24 16:09:09 -0800

    remove coffee, precompile assets

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 Bundler.require(:default,:assests,Rails.env)
+Bundler.require(*Rails.groups(assets: %w(development test production)))
+
 module Landing3
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
