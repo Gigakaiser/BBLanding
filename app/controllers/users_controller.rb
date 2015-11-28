d3d5eb2ba143b5e51c2bbbce494fb9121ceb4f49
Gigakaiser <Brandon.Hjelstrom@gmail.com>
2015-11-27 17:19:34 -0800

    Successfully adding new users to DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,28 @@
 class UsersController < ApplicationController
 	def home
-			session[:refid] = nil
-			@referrer = nil
+		session[:refid] = nil
+		@referrer = nil
 			@newuser = User.new
 	end
 	def referral_signup
 		session[:refid] = params[:userid]
 		@referrer = User.where(userid: params[:userid]).first
-		@newuser = User.new
 		render 'home'
+		@newuser = User.new
 	end
-
 	
+	def create
+		 @user = User.new(user_params)
+    if @user.save
+      redirect_to '/users'
+    else
+       redirect_to '/'
+		end
+	
+	end
+	def update
+			redirect_to '/'
+	end
 
 	def index
 		@users = User.all
@@ -20,5 +31,8 @@ class UsersController < ApplicationController
 		@user = User.where(userid: params[:userid]).first
 		@userrefs = User.where(referrerid: params[:userid])
 		@refcount = @userrefs.length;
+	end
+	def user_params
+		 params.require(:user).permit(:email)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	belongs_to :referred_by, :class_name => "User"
 	has_many :referrals, :class_name => "User"
-	validates_uniqueness_of :email
+	#validates_uniqueness_of :email
 	
 end
