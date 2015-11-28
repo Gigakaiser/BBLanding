770f0f51d8782f2477a22f07499ef8298d07c8ee
Gigakaiser <Brandon.Hjelstrom@gmail.com>
2015-11-27 18:29:39 -0800

    fix migration order

--- db/migrate/20151126072654_create_users.rb
+++ db/migrate/20151126072654_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :email
     	t.string :userid
-    	t.string :referrerid
       t.timestamps
     end
   end
